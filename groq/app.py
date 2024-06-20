2c2ccbabb8cb38aa32a5c9c99ed10c770a863dfd
edgar-nbc <2512@eternalai.org>
2024-06-20 10:28:25 +0700

    groq

---
+++ groq/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_community.vectorstores import FAISS 
+import time
+
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.environ["GROQ_API_KEY"]
+
